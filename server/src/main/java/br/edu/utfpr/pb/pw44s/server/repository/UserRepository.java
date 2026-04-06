4455e19beff5bd93a1c292b18593166c3c9ed718
mazeta1910 <matheuscps110@gmail.com>
2026-04-06 20:25:15 -0300

    Move server into Aulas/pw44s-main; add new server
    Reorganize project layout by moving the existing server project into Aulas/pw44s-main (including client and server subfolders and their sources). Add IntelliJ .idea config and a .gitignore for the pw44s-main workspace. Introduce a new top-level server/ directory with Maven wrapper, pom.xml and a basic Java application skeleton (Application, controllers, services, repositories and tests) to provide a fresh server module.

---
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.pw44s.server.repository;
+
+import br.edu.utfpr.pb.pw44s.server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    //SELECT user.* FROM user WHERE username == ?
+    User findUserByUsername(String username);
+
+}
